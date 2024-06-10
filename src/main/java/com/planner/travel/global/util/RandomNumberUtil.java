043f67aca893fc59c75c257ba86a4d3c456a6ead
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-10 16:44:16 +0900

    Feat: 메일 인증 기능 구현 (#3)

--- src/main/java/com/planner/travel/global/util/RandomNumberUtil.java
+++ src/main/java/com/planner/travel/global/util/RandomNumberUtil.java
@@ -11,13 +11,19 @@ import java.util.Random;
 public class RandomNumberUtil {
     private final UserRepository userRepository;
 
-    public Long set() {
+    public Long setUserCode() {
         Random random = new Random();
         long randomNumber = random.nextLong(9000) + 1000;
 
         while (userRepository.findByUserTag(randomNumber).isPresent()) {
             randomNumber = random.nextLong(9000) + 1000;
         }
+        return randomNumber;
+    }
+
+    public Long setTempCode() {
+        Random random = new Random();
+        long randomNumber = random.nextLong(900000) + 100000;
         return randomNumber;
     }
 }
