bcde85333bea0b3178d8ea0344a229e7916e7bbf
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-19 21:39:55 +0900

    Fix: 플랜 박스 생성 요청 시 필요 없는 필드 제거 (#6)

--- src/main/java/com/planner/travel/domain/planner/dto/request/PlanBoxCreateRequest.java
+++ src/main/java/com/planner/travel/domain/planner/dto/request/PlanBoxCreateRequest.java
@@ -2,5 +2,5 @@ package com.planner.travel.domain.planner.dto.request;
 
 import java.time.LocalDate;
 
-public record PlanBoxCreateRequest(LocalDate planDate, boolean isPrivate) {
+public record PlanBoxCreateRequest(LocalDate planDate) {
 }
