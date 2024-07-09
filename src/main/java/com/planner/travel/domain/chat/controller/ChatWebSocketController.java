d070c036587db1f5e50449d5e822720c5ac06ef2
sieunnnn <wldsmtldsm65@gmail.com>
2024-07-09 16:32:11 +0900

    Feat: 채팅 annotaion, dto 수정 (#6)

--- src/main/java/com/planner/travel/domain/chat/controller/ChatWebSocketController.java
+++ src/main/java/com/planner/travel/domain/chat/controller/ChatWebSocketController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ChatWebSocketController {
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private final TokenAuthenticator tokenAuthenticator;
 
     @MessageMapping(value = "/planner/{plannerId}/chat/send")
     public void sendMessage(
             @DestinationVariable Long plannerId,
-            @RequestBody ChatDto chatDto) {
+            @Payload ChatDto chatDto) {
 
         simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
                 Map.of("type", "chat", "message", chatDto)
