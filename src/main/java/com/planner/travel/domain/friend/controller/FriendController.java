36861564e19f76173782e2aec3d267f04c7b8981
sieunnnn <wldsmtldsm65@gmail.com>
2024-07-18 10:38:09 +0900

    Feat: 친구 기능 컨트롤러, 쿼리 서비스 구현 중 (#5)

---
+++ src/main/java/com/planner/travel/domain/friend/controller/FriendController.java
@@ -0,0 +1,10 @@
+package com.planner.travel.domain.friend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/friends")
+public class FriendController {
+
+}
---
+++ src/main/java/com/planner/travel/domain/friend/query/FriendQueryService.java
@@ -0,0 +1,6 @@
+package com.planner.travel.domain.friend.query;
+
+public class FriendQueryService {
+    // 승낙 기다리는 친구 리스트
+    // 친구 리스트 출력
+}
